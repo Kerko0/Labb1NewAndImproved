d54873ef32771a8971fb101d5718583ce7dc2029
Kerko0 <eric.kjelldorff@telia.com>
2026-09-09 13:09:36 +0200

    Moved function to be part of constructor instead

--- Labb1/IndexedString.cs
+++ Labb1/IndexedString.cs
@@ -3,22 +3,16 @@ namespace Labb1;
 public class IndexedString
 {
     private readonly string _inputString;
-    private readonly List<Index> _indexes;
+    private readonly List<Index> _indexes = new List<Index>();
     
     public IndexedString (string input)
     {
         _inputString = input;
-        _indexes = IndexString(input);
-    }
-    
-    private List<Index> IndexString(string input)
-    {
-        List<Index> indexes = new List<Index>();
         
-        for (int i = 0; i < input.Length; i++)
+        for (int i = 0; i < _inputString.Length; i++)
         {
-            char currentChar =  input[i];
-            string substringToSearch = input.Substring(i+1, input.Length-i-1);
+            char currentChar =  _inputString[i];
+            string substringToSearch = _inputString.Substring(i+1, _inputString.Length-i-1);
             
             int matchingCharIndexOf = substringToSearch.IndexOf(currentChar);
             if (matchingCharIndexOf != -1)
@@ -26,16 +20,14 @@ public class IndexedString
                 int startIndex = i;
                 int length = matchingCharIndexOf + 2;
                 
-                if (input.Substring(startIndex, length).Any(Char.IsLetter))
+                if (_inputString.Substring(startIndex, length).Any(Char.IsLetter))
                 {
                     continue;
                 }
                 
-                indexes.Add(new Index(startIndex, length));
+                _indexes.Add(new Index(startIndex, length));
             }
         }
-
-        return indexes;
     }
 
     public void PrintAndColor()
