124233d601c0bc9195c9fe6b114d52cf9ca08e6b
Kerko0 <eric.kjelldorff@telia.com>
2026-09-09 13:57:48 +0200

    Added variant without classes

---
+++ Labb1_noclasses/Program.cs
@@ -0,0 +1,47 @@
+﻿long sum = 0;
+string input;
+
+do
+{
+    Console.WriteLine("Please input a random string of numbers and letters.");
+    input = Console.ReadLine();
+            
+} while (string.IsNullOrEmpty(input));
+
+for (int i = 0; i < input.Length; i++)
+{
+    char numToMatch =  input[i];
+    string substringToSearch = input.Substring(i+1, input.Length-i-1);
+            
+    int matchingNumIndexOf = substringToSearch.IndexOf(numToMatch);
+    if (matchingNumIndexOf != -1)
+    {
+        int startIndex = i;
+        int length = matchingNumIndexOf + 2;
+                
+        if (input.Substring(startIndex, length).Any(Char.IsLetter))
+        {
+            continue;
+        }
+                
+        for (int j = 0; j < input.Length; j++)
+        {
+            if (Enumerable.Range(startIndex, length).Contains(j))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.Write(input[j]);
+        }
+        
+        Console.WriteLine();
+        
+        sum += Convert.ToInt64(input.Substring(startIndex, length));      
+    }
+}
+
+Console.WriteLine($"Sum of all numbers marked in red: {sum}");
+
